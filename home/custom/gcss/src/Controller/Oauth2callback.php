2b372ccbebda387ff2e38de9c3cd206711513552
Ashwin Shaharkar <ashwin.shaharkar@gmail.com>
2018-08-12 22:49:30 +0530

    A

--- home/custom/gcss/src/Controller/Oauth2callback.php
+++ home/custom/gcss/src/Controller/Oauth2callback.php
@@ -6,9 +6,9 @@ use Drupal\Core\Controller\ControllerBase;
 use Google_Client;
 use Google_Service_ShoppingContent;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+
 define('DRUPAL_ROOT', dirname(dirname(__DIR__)));
 
-//echo DRUPAL_ROOT; die;
 /**
  * Provides route responses for the Example module.
  */
@@ -36,7 +36,7 @@ class Oauth2callback extends ControllerBase
         // Create the client object and set the authorization configuration
         // from the client_secrets.json you downloaded from the Developers Console.
         $client = new Google_Client();
-        $client->setAuthConfig(DRUPAL_ROOT."\client_secret_43212426181-1g65j41m17r5418ugr707qun3u66gtfv.apps.googleusercontent.com.json");
+        $client->setAuthConfig(DRUPAL_ROOT . "\client_secret_43212426181-1g65j41m17r5418ugr707qun3u66gtfv.apps.googleusercontent.com.json");
         $client->setRedirectUri("$client_oauthCallbackUrl");
         $client->setScopes(Google_Service_ShoppingContent::CONTENT);
 
